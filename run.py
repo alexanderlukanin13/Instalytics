a79d9f57f22e0362fb7921df07c53f05396a382a
Gregor von Bergen <gregor@vbergen.ch>
2019-01-01 11:48:12 +0100

    DB items updated

--- run.py
+++ run.py
@@ -94,7 +94,7 @@ if __name__ == '__main__':
         if locationretrieved == True:
             ex.location_details(args.key)
         else:
-            logging.info('%s: Location skipped')
+            logging.info('%s: Location skipped', args.key)
 
     # one pictures
     elif args.command == 'get' and args.category == 'picture':
