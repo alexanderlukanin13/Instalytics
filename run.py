4319999b58541b01e1487de83f407eb65d16fc37
Alexander Lukanin <alexander.lukanin.13@gmail.com>
2019-01-08 21:34:39 +0500

    run.py: parse_args() and main()

--- run.py
+++ run.py
@@ -11,6 +11,10 @@ from app import Retrieve
 from app import Search
 from app import Extract
 
+
+retr = None
+
+
 def mp_retrieve_location(location_list):
     """
     Function to support multiprocessing and avoid getting a pickle error for locations
@@ -54,30 +58,37 @@ def mp_retrieve_user(user_list):
     retr.retrieve_user(user_dictionary['username'])
 
 
-parser = argparse.ArgumentParser()
-subparser = parser.add_subparsers()
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser()
+    subparser = parser.add_subparsers()
 
-# Parser for running one-off searches / test
-parser_get = subparser.add_parser('get', )
-parser_get.add_argument('category', choices=('location', 'user', 'picture'),
-                         help='Define the category that you want to search, e.g. location',)
-parser_get.add_argument('key', help='Give the key you want to search, e.g 39949930 (for location)')
-parser_get.set_defaults(command='get')
+    # Parser for running one-off searches / test
+    parser_get = subparser.add_parser('get', )
+    parser_get.add_argument('category', choices=('location', 'user', 'picture'),
+                            help='Define the category that you want to search, e.g. location',)
+    parser_get.add_argument('key',
+                            help='Give the key you want to search, e.g 39949930 (for location)')
+    parser_get.set_defaults(command='get')
 
-# Parser for running the program
-parser_run = subparser.add_parser('run')
-parser_run.add_argument('category', choices=('location', 'user', 'picture'))
-parser_run.set_defaults(command='run')
+    # Parser for running the program
+    parser_run = subparser.add_parser('run')
+    parser_run.add_argument('category', choices=('location', 'user', 'picture'))
+    parser_run.set_defaults(command='run')
+
+    return parser.parse_args()
 
 
-if __name__ == '__main__':
+def main():
+    """Main function of the script."""
     logging.basicConfig(level=logging.INFO)
-    args = parser.parse_args()
+    args = parse_args()
 
     logging.info(args.__dict__)
 
     # Initialize profiles
     sr = Search()
+    global retr
     retr = Retrieve(useproxy=True, awsprofile='default', storage_directory='.')
     ex = Extract(awsprofile='default', storage_directory='.')
     dynamo = boto3.resource('dynamodb')
@@ -218,3 +229,7 @@ if __name__ == '__main__':
 
     else:
         logging.info('No valid category')
+
+
+if __name__ == '__main__':
+    main()
